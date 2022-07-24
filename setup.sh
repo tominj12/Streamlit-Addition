93fd01623ee166146b3a6d339c2133317defecaf
ROZINA SAHERWALA <21f1006372@student.onlinedegree.iitm.ac.in>
2022-07-24 18:02:31 +0530

    Updated Setup.sh`:wq!

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"rohith@onlinedegree.iitm.ac.in\"\n\
+email = \"21f1006372@student.onlinedegree.iitm.ac.in\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
