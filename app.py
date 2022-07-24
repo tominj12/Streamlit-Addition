926c3b477bba0004fd65f3a93cc392746dc48e04
ROZINA SAHERWALA <21f1006372@student.onlinedegree.iitm.ac.in>
2022-07-24 18:48:23 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -16,5 +16,4 @@ st.header('User Input Parameters')
 No1 = st.number_input("NO_1 ",min_value=0.0,max_value=2000000.0)
 No2 = st.number_input("NO_2 ",min_value=0.0,max_value=2000000.0)
 result= No1 - No2
-st.subheader('Subtraction')
 st.write(No1 ,' - ', No2,' = ',result)
