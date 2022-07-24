7440a5355460e97547ee872723400f46d4b58795
ROZINA SAHERWALA <21f1006372@student.onlinedegree.iitm.ac.in>
2022-07-24 19:27:33 +0530

    Added New app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import streamlit as st
 
-string = "Subtraction of Two Numbers"
-st.set_page_config(page_title=string, page_icon="➕")
-st.title('Subtraction of Two Numbers')
-#x = st.number_input('Enter a number')
-num1 = st.number_input("Enter first number",min_value=0.0,max_value=100000000.0)
-num2 = st.number_input("Enter second number",min_value=0.0,max_value=100000000.0)
-if (num1).is_integer() and (num2).is_integer():
-    sum=num1-num2
-    st.write("the addition result is", sum)
-else:st.write("please enter integer number")
+st.title('Substraction of two numbers')
+st.subheader('Rozina Saherwala')
+st.text('21f1006372')
+
+num1 = st.text_input('Enter first number : ')
+num2 = st.text_input('Enter second number : ')
+
+btn = st.button('Calculate')
+
+
+if btn:
+    if num1 and num2:
+        try:
+            num1 = int(num1)
+            num2 = int(num2)
+            st.markdown(f"## {num1} - {num2} = {float(num1) - float(num2)}")
+        except ZeroDivisionError:
+            st.error("Do not divide by zero !!!")
+        except ValueError:
+            st.error("Enter numbers only !!!")
+    else:
+        st.error('Please enter numbers')
