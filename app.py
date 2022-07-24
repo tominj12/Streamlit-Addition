f6c141bcb02bc63e74e65f21f4da4158ddbb4e6a
Tomin J Vellackal <107436153+tominj12@users.noreply.github.com>
2022-07-24 22:46:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-st.title('Substraction of two numbers')
-st.subheader('Rozina Saherwala')
-st.text('21f1006372')
+st.title('Addition of two numbers')
+st.subheader('Tomin J Vellackal')
+st.text('21f1004496')
 
-num1 = st.text_input('Enter first number : ')
-num2 = st.text_input('Enter second number : ')
+num1 = st.text_input('Enter the first number : ')
+num2 = st.text_input('Enter the second number : ')
 
 btn = st.button('Calculate')
 
@@ -15,10 +15,10 @@ if btn:
         try:
             num1 = float(num1)
             num2 = float(num2)
-            st.markdown(f"## {num1} - {num2} = {float(num1) - float(num2)}")
+            st.markdown(f"## {num1} + {num2} = {float(num1) + float(num2)}")
         except ZeroDivisionError:
-            st.error("Do not divide by zero !!!")
+            st.error("Do not divide by zero!")
         except ValueError:
-            st.error("Enter numbers only !!!")
+            st.error("Enter numbers only!")
     else:
-        st.error('Please enter numbers')
+        st.error('Please enter the numbers')
