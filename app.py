2d0e6e48b95675d9a93e89e52059ef3104622c3b
ROZINA SAHERWALA <21f1006372@student.onlinedegree.iitm.ac.in>
2022-07-24 19:13:04 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 string = "Subtraction of Two Numbers"
 st.set_page_config(page_title=string, page_icon="➕")
-st.title('2 Numebers Addition')
+st.title('Subtraction of Two Numbers')
 #x = st.number_input('Enter a number')
 num1 = st.number_input("Enter first number",min_value=0.0,max_value=100000000.0)
 num2 = st.number_input("Enter second number",min_value=0.0,max_value=100000000.0)
